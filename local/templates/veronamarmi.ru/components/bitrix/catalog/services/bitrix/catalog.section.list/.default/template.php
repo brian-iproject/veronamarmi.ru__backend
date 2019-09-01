9714d1c7a4bed4b535e9ecb22fbf423807c65033
Алексей Павлов <brian.iproject@gmail.com>
2019-09-01 23:04:00 +0300

    Правка отступа в разделе услуг

--- local/templates/veronamarmi.ru/components/bitrix/catalog/services/bitrix/catalog.section.list/.default/template.php
+++ local/templates/veronamarmi.ru/components/bitrix/catalog/services/bitrix/catalog.section.list/.default/template.php
@@ -12,19 +12,21 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="services-list">
-  <?
-  foreach ($arResult['SECTIONS'] as $arSection) {
-      $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], CIBlock::GetArrayByID($arSection["IBLOCK_ID"], "SECTION_EDIT"));
-      $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], CIBlock::GetArrayByID($arSection["IBLOCK_ID"], "SECTION_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-      ?>
-    <div class="services-list__item" id="<?=$this->GetEditAreaId($arSection['ID']);?>">
-	    <a href="<?=$arSection["SECTION_PAGE_URL"]?>" class="services-list__link">
-		    <? if ($arSection["PX_PICTURE"]["SRC"]) { ?>
-		      <span class="services-list__image"><img src="<?=$arSection["PX_PICTURE"]["SRC"]?>" alt="<?=$arSection["PICTURE"]["ALT"]?>"></span>
-		    <? } ?>
-        <?=$arSection["NAME"]?>
-	    </a>
-    </div>
-    <? } ?>
-</div>
+<? if (count($arResult['SECTIONS'])) { ?>
+	<div class="services-list">
+	  <?
+	  foreach ($arResult['SECTIONS'] as $arSection) {
+	      $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], CIBlock::GetArrayByID($arSection["IBLOCK_ID"], "SECTION_EDIT"));
+	      $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], CIBlock::GetArrayByID($arSection["IBLOCK_ID"], "SECTION_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	      ?>
+	    <div class="services-list__item" id="<?=$this->GetEditAreaId($arSection['ID']);?>">
+		    <a href="<?=$arSection["SECTION_PAGE_URL"]?>" class="services-list__link">
+			    <? if ($arSection["PX_PICTURE"]["SRC"]) { ?>
+			      <span class="services-list__image"><img src="<?=$arSection["PX_PICTURE"]["SRC"]?>" alt="<?=$arSection["PICTURE"]["ALT"]?>"></span>
+			    <? } ?>
+	        <?=$arSection["NAME"]?>
+		    </a>
+	    </div>
+	    <? } ?>
+	</div>
+<? } ?>
