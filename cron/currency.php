5173add337804e7240f59c12ffa742e98c3ad032
Алексей Павлов <brian.iproject@gmail.com>
2019-09-30 11:53:40 +0300

    Курсы валют

---
+++ cron/currency.php
@@ -0,0 +1,64 @@
+<?
+$_SERVER["DOCUMENT_ROOT"] = "/home/mramorland/veronamarmi.ru/docs";
+define("STOP_STATISTICS", true);
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/classes/general/xml.php');
+
+global $USER;
+global $APPLICATION;
+$arResult = array();
+
+use Bitrix\Main\Loader;
+Loader::includeModule("highloadblock");
+use Bitrix\Highloadblock as HL;
+use Bitrix\Main\Entity;
+
+$hlbl = 3; // Указываем ID нашего highloadblock блока к которому будет делать запросы.
+$hlblock = HL\HighloadBlockTable::getById($hlbl)->fetch();
+
+$entity = HL\HighloadBlockTable::compileEntity($hlblock);
+$entity_data_class = $entity->getDataClass();
+
+$rsData = $entity_data_class::getList(array(
+    "select" => array("*"),
+    "order" => array(),
+    "filter" => array()  // Задаем параметры фильтра выборки
+));
+
+while($arData = $rsData->Fetch()) {
+    $arResult['CURRENCIES'][$arData['UF_XML_ID']] = $arData;
+}
+
+$strQueryText = QueryGetData(
+    "www.cbr.ru",
+    '443',
+    "/scripts/XML_daily.asp",
+    false,
+    $error_number,
+    $error_text,
+    "GET",
+    "ssl://"
+);
+
+$xml = new SimpleXMLElement($strQueryText);
+
+foreach ($xml->Valute as $currency) {
+    $currency = get_object_vars($currency);
+    $arResult['CBRF_CURRENCIES'][$currency['CharCode']] = $currency;
+}
+
+
+foreach ($arResult['CBRF_CURRENCIES'] as $currencyCode => $currency) {
+    $data = array(
+        "UF_VALUE"=> $currency['Value']/$currency['Nominal'],
+        "UF_NAME"=> $currency['Name'],
+        "UF_XML_ID"=> $currency['CharCode']
+    );
+
+    if ($arResult['CURRENCIES'][$currencyCode]) {
+        $result = $entity_data_class::update($arResult['CURRENCIES'][$currencyCode]['ID'], $data);
+    } else {
+        $result = $entity_data_class::add($data);
+    }
+}
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php");?>
